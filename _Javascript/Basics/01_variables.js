0da12fc04c77aef28a904eb9d0f504e1c30879b5
Dipalinikam123 <127381417+Dipalinikam123@users.noreply.github.com>
2025-06-11 10:49:02 +0000

    conversion operator

---
+++ _Javascript/Basics/01_variables.js
@@ -0,0 +1,31 @@
+const accountId = 10202;
+let accountEmail = "abc@gmail.com";
+var accountName = "abc"
+city = "surat"
+let accountStatus;
+
+
+
+console.table([accountId,accountEmail,accountName,city,accountStatus])
+
+// accountId = 232323  // cn not change constant variable
+
+accountEmail="xyz@gmail.com"
+accountName ="xyz"
+city="Daman"
+
+console.table([accountId,accountEmail,accountName,city,accountStatus])
+
+// ------- Data Type -------------
+//  ----- 1 Primitive ----------
+//   number => 2 to power 53
+//   bigInt
+//   string =""
+//   boolean = true/false
+//   null = value that not undefine
+//   undefine
+//   symbol => unique
+
+//  ----- 2 non Primitive ----------
+
+
