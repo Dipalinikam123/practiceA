edc1a7278f32d20246041f62967e1f0c735598c3
Dipalinikam123 <127381417+Dipalinikam123@users.noreply.github.com>
2025-06-12 07:11:16 +0000

    string and math

--- _Javascript/Basics/04_dataType.js
+++ _Javascript/Basics/04_dataType.js
@@ -34,9 +34,35 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++ Memory store +++++++++++++++++++++++++++++++++++++++++++++
+
+// stack(primitive) and Heap (non-primitive)
+// this primitive data store in stack flow which assign a copy of variable value ,so if we change in name2 that can not effect on name
+
+let name= "artoon";
+let name2=name;
+
+name2="superwork"
+
+console.log(name)
+console.log(name2)
+
+//  this non primitive datatypes store in heap memory that can pass the referance of value 
+let personOne={
+    email:"abc@gmail.com"
+}
+
+let personTwo=personOne
+personTwo.email="xyz@gmail.com"
+
+console.log(personOne);
+console.log(personTwo);
+
